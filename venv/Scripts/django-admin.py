340b2e5febf41276b9c68ce36d2a1ed05660d1ed
YoonJ <oyoons95@gmail.com>
2018-09-21 14:26:58 +0900

    first commit~

---
+++ venv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\dizi9_3cpmuxu\PycharmProjects\finalProject\venv\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
